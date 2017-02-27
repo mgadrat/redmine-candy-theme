3b2aca708ec045199dca5234838bb557202dbb66
Matthieu Gadrat <mgadrat@gmail.com>
2017-02-27 12:44:57 -0500

    reconfigure gulp.js

--- tools/Gulpfile.js
+++ tools/Gulpfile.js
@@ -2,29 +2,39 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var notify = require("gulp-notify");
 var concat = require('gulp-concat');
+var runSequence = require('run-sequence');
 
-gulp.task('css-compile', function() {
+
+gulp.task('styles', function() {
+  runSequence('custom-styles', 'add-vendor-css');
+});
+
+gulp.task('scripts', function() {
+  runSequence('custom-scripts', 'add-vendor-js');
+});
+
+gulp.task('custom-styles', function() {
   gulp.src('../sass/**/*.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('../themes/gadmine/stylesheets/'))
     .pipe(notify("Your css is ready!"));
 });
 
-gulp.task('js-compile', function() {
+gulp.task('custom-scripts', function() {
   gulp.src('../js/**/*.js')
     .pipe(concat('theme.js'))
     .pipe(gulp.dest('../themes/gadmine/javascripts/'))
     .pipe(notify("Your js is ready!"));
 });
 
-gulp.task('css-combine', ['css-compile'], function() {
+gulp.task('add-vendor-css', function() {
   gulp.src(['../vendor/select2.min.css', '../themes/gadmine/stylesheets/application.css'])
     .pipe(concat('application.css'))
     .pipe(gulp.dest('../themes/gadmine/stylesheets/'))
     .pipe(notify("Vendors css is ready!"));
 })
 
-gulp.task('js-combine', ['js-compile'], function() {
+gulp.task('add-vendor-js', function() {
   gulp.src(['../vendor/select2.full.min.js', '../themes/gadmine/javascripts/theme.js'])
     .pipe(concat('theme.js'))
     .pipe(gulp.dest('../themes/gadmine/javascripts/'))
@@ -33,6 +43,6 @@ gulp.task('js-combine', ['js-compile'], function() {
 
 //Watch task
 gulp.task('default',function() {
-    gulp.watch('../sass/**/*.scss',['css-combine']);
-    gulp.watch('../js/**/*.js',['js-combine']);
+    gulp.watch('../sass/**/*.scss',['styles']);
+    gulp.watch('../js/**/*.js',['scripts']);
 });
