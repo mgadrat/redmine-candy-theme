5ffc68bd13cbd15aaf9e47ef678b6cc83ae89254
Matthieu Gadrat <mgadrat@gmail.com>
2017-03-01 22:34:54 -0500

    reattach select 2 for ajax calls

--- js/theme.js
+++ js/theme.js
@@ -1,3 +1,10 @@
 $(function() {
-  $('select').select2();
+
+  // cant't fiend a better way :( sorry)
+  reattach_select2();
+  function reattach_select2() {
+    $('select:not(.select2-hidden-accessible)').select2();
+    setTimeout( reattach_select2, 100 );
+  }
+
 });
